facb7cf777be05b586140369eb79c48dbbdfd93d
Weleyalego <onyaloalego@gmail.com>
2023-08-14 02:26:51 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-
 /**
- * main - prints "programming is like building a multilingual puzzle"
- * return 0.
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
