f8aa163f96d38a032523d14d9550ba26e27ea589
Weleyalego <onyaloalego@gmail.com>
2023-09-04 04:36:41 -0700

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion - Prints a string, followed by a new line.
+ * _print_rev_recursion - Prints a string in reverse, followed by a new line.
  * @s: the string to be printed
  *
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (s[0] != '\0')
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(s[0]);
-		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
